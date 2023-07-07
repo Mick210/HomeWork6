ec6e53c573c4d11a63b31e54ce5768f78de268c7
Mick210 <sid222m@gmail.com>
2023-07-07 08:24:43 +0300

    Доработки внесены

--- src/Main.java
+++ src/Main.java
@@ -10,54 +10,53 @@ public class Main {
         task8();
         task9();
         task10();
-
     }
 
     public static void task1() {
         System.out.println("Задача 1");
-        for (int i = 1; i <= 10; i = i + 1) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        for (int i = 10; i >= 1; i = i - 1) {
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
 
     public static void task3() {
         System.out.println("Задача 3");
-        for (int i = 0; i <= 17; i = i + 2) {
+        for (int i = 0; i <= 17; i += 2) {
             System.out.println(i);
         }
     }
 
     public static void task4() {
         System.out.println("Задача 4");
-        for (int i = 10; i >= -10; i = i - 1) {
+        for (int i = 10; i >= -10; i -= 1) {
             System.out.println(i);
         }
     }
 
     public static void task5() {
         System.out.println("Задача 5");
-        for (int i = 1904; i < 2096; i = i + 4) {
+        for (int i = 1904; i <= 2096; i += 4) {
             System.out.println(i + " год является високосным");
         }
     }
 
     public static void task6() {
         System.out.println("Задача 6");
-        for (int i = 7; i <= 98; i = i + 7) {
+        for (int i = 7; i <= 98; i += 7) {
             System.out.println(i);
         }
     }
 
     public static void task7() {
         System.out.println("Задача 7");
-        for (int i = 1; i <= 512; i = i * 2) {
+        for (int i = 1; i <= 512; i *= 2) {
             System.out.println(i);
         }
     }
@@ -76,9 +75,11 @@ public class Main {
         System.out.println("Задача 9");
         float salary = 29000f;
         float savings = 0f;
+        int yearPercent = 12;
+        int monthPercent = yearPercent / 12;
         float percentages = 0f;
         for (int i = 1; i <= 12; i++) {
-            percentages = savings / 100;
+            percentages = savings * monthPercent / 100;
             savings = savings + salary + percentages;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
         }
@@ -91,9 +92,6 @@ public class Main {
             int a = i * 2;
             System.out.println("2*" + i + "=" + a);
         }
-
-
     }
-
 }
 
